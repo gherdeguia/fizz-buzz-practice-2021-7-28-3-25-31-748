0d04424031230b4f9165fc302e52a5e34996bd6b
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-05 14:12:23 +0800

    [FizzBuzzExercise][Gerarld] refactor : refactor codes and expectedResult variable for code reuse

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -7,6 +7,9 @@ public class FizzBuzz {
         if(givenNumber % 3 == 0){
             return "Fizz";
         }
+        else if(givenNumber % 5 == 0){
+            return "Buzz";
+        }
         else{
             return String.valueOf(givenNumber);
         }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -17,29 +17,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class FizzBuzzTest {
+
+    String expectedResult;
+
     @Test
     void should_return_order_number_when_count_off_given_normal_case_order_number(){
         //given
         int givenNumber = 1;
+        expectedResult = "1";
         FizzBuzz fizzbuzz = new FizzBuzz();
 
         //when
         String result = fizzbuzz.countOff(givenNumber);
 
         //then
-        assertThat(result).isEqualTo("1");
+        assertThat(result).isEqualTo(expectedResult);
     }
 
     @Test
     void should_return_fizz_when_count_off_given_multiple_of_3() {
         //given
         int givenNumber = 3;
+        expectedResult = "Fizz";
         FizzBuzz fizzbuzz = new FizzBuzz();
 
         //when
         String result = fizzbuzz.countOff(givenNumber);
 
         //then
-        assertThat(result).isEqualTo("Fizz");
+        assertThat(result).isEqualTo(expectedResult);
     }
+
+    @Test
+    void should_return_buzz_when_count_off_given_multiple_of_5() {
+        //given
+        int givenNumber = 5;
+        expectedResult = "Buzz";
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        //when
+        String result = fizzbuzz.countOff(givenNumber);
+
+        //then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+
 }
